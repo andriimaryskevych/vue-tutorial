adc3a9a9b306dc0b1c14f4bdc5d663ba6903eef2
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-05 16:55:09 +0300

    Add event handlers

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ new Vue({
     el: '#vue-app',
     data: {
         title: 'Vue tutor',
-        name: 'Andrii',
-        url: 'https://www.youtube.com/',
-        classes: ['one', 'two'],
+        wage: 10,
     },
     methods: {
-        greet(time) {
-            return `Hello and good ${time}, ${this.name}`;
-        },
+        changeWage(amount) {
+            this.wage += amount;
+        }
     }
 });
