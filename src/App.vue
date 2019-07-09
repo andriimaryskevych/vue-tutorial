4617917a790618c785c4aabaf3d94675967b48cc
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-09 18:00:31 +0300

    Add navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar';
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      title: 'My first Vue.js App'
+    name: 'app',
+    components: {
+        Navbar
+    },
+    data () {
+        return {
+        title: 'My first Vue.js App'
+        }
     }
-  }
 }
 </script>
 
---
+++ src/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <h2>Menu</h2>
+
+        <li><a href="">Home</a></li>
+        <li><a href="">Acout</a></li>
+        <li><a href="">Contact</a></li>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+    nav {
+        text-align: center;
+    }
+
+    nav ul {
+        padding: 0;
+    }
+
+    nav li {
+        display: inline-block;
+        list-style-type: none;
+        margin: 0;
+    }
+</style>
