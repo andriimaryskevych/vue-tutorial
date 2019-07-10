abbbb8edae33b1502a8426ff287018c8ad1c8e68
Andrii Maryskevych <andrii.maryskevych@globallogic.com>
2019-07-10 11:35:14 +0300

    Add test lifecycle component

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,23 @@
     <div id="app">
         <h1>{{ title }}</h1>
         <Navbar />
-        <AllFriends :friends="friends" @delete="deleteFriend" />
-        <OnlineFriends :friends="friends" @delete="deleteFriend" />
+        <Blogs />
     </div>
 </template>
 
 <script>
 import Navbar from './Navbar';
-import AllFriends from './AllFriends';
-import OnlineFriends from './OnlineFriends';
+import Blogs from './Blogs';
 
 export default {
     name: 'app',
     components: {
         Navbar,
-        AllFriends,
-        OnlineFriends,
+        Blogs,
     },
     data () {
         return {
             title: 'My first Vue.js App',
-            friends: [
-                { name: 'Mario', online: true },
-                { name: 'Abigale', online: false },
-                { name: 'Den', online: true },
-                { name: 'Mark', online: false },
-                { name: 'Olena', online: true },
-            ],
         }
     },
     methods: {
---
+++ src/Blogs.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="blogs">
+        <h2></h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blogs',
+    data() {
+        return {
+            blogTitle: 'Blogs',
+        }
+    },
+    beforeCreate() {
+        console.log('Before create hook');
+    },
+    created() {
+        console.log('Created hook');
+    },
+}
+</script>
+
